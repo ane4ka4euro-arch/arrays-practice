f6dca501d077bc6a740a5bdbc0037846ffeb21e7
Рузавина Анна <ane4ka4euro@gmail.com>
2026-09-17 15:50:50 +0300

    Task2: ввод, реверс и сортировка массива

--- Task2.InputSort/Program.cs
+++ Task2.InputSort/Program.cs
@@ -1,97 +1,86 @@
 ﻿using System;
 
-namespace LabWork
+int n = 0;
+
+while (n <= 0)
 {
-    class Program
+    Console.Write("Введите количество элементов: ");
+
+    try
     {
-        static void Main(string[] args)
+        n = int.Parse(Console.ReadLine() ?? "");
+
+        if (n <= 0)
         {
-            int N = 0;
-            bool ok = false;
-            while (!ok)
-            {
-                Console.Write("Введите кол-во элементов: ");
-                try
-                {
-                    N = int.Parse(Console.ReadLine() ?? "");
-                    if (N > 0)
-                        ok = true;
-                    else
-                        Console.WriteLine("Ошибка: N должно быть больше 0!");
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Ошибка: введено не число!");
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Ошибка: число слишком большое!");
-                }
-            }
-
-            int[] arr = new int[N];
-
-            for (int i = 0; i < N; i++)
-            {
-                bool okElem = false;
-                while (!okElem)
-                {
-                    Console.Write("Элемент [" + i + "]: ");
-                    try
-                    {
-                        arr[i] = int.Parse(Console.ReadLine() ?? "");
-                        okElem = true;
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Ошибка: введено не число!");
-                    }
-                    catch (OverflowException)
-                    {
-                        Console.WriteLine("Ошибка: число слишком большое!");
-                    }
-                }
-            }
-
-            Console.WriteLine();
-
-            Console.Write("Исходный массив:  ");
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write(arr[i]);
-                if (i < N - 1) Console.Write(", ");
-            }
-            Console.WriteLine();
-
-            Console.Write("Обратный порядок: ");
-            for (int i = N - 1; i >= 0; i--)
-            {
-                Console.Write(arr[i]);
-                if (i > 0) Console.Write(", ");
-            }
-            Console.WriteLine();
-
-            Array.Sort(arr);
-            Console.Write("Отсортированный:  ");
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write(arr[i]);
-                if (i < N - 1) Console.Write(", ");
-            }
-            Console.WriteLine();
-
-            int max = arr[0];
-            int min = arr[0];
-            for (int i = 1; i < N; i++)
-            {
-                if (arr[i] > max) max = arr[i];
-                if (arr[i] < min) min = arr[i];
-            }
-
-            Console.WriteLine("Максимум: " + max);
-            Console.WriteLine("Минимум: " + min);
-
-            Console.ReadKey();
+            Console.WriteLine("Количество должно быть больше 0.");
         }
     }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ошибка ввода.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Ошибка ввода.");
+    }
 }
+
+int[] numbers = new int[n];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    try
+    {
+        Console.Write("Элемент [" + i + "]: ");
+        numbers[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ошибка ввода.");
+        i--;
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Ошибка ввода.");
+        i--;
+    }
+}
+
+Console.WriteLine("Исходный массив: " + string.Join(", ", numbers));
+
+Console.Write("Обратный порядок: ");
+
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    Console.Write(numbers[i]);
+
+    if (i > 0)
+    {
+        Console.Write(", ");
+    }
+}
+
+Console.WriteLine();
+
+Array.Sort(numbers);
+
+Console.WriteLine("Отсортированный массив: " + string.Join(", ", numbers));
+
+int min = numbers[0];
+int max = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+}
+
+Console.WriteLine("Минимум: " + min);
+Console.WriteLine("Максимум: " + max);
